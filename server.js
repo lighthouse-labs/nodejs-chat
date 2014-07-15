10d5a0538cd527144d3c9dc2065d1a181408da45
Monica Olinescu <monica.olinescu@gmail.com>
2014-07-15 01:00:51 -0700

    Clients can connect and disconnect from socket

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var _ = require('underscore');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-var nodes = { };
-var usernames = {};
 server.listen(process.env.PORT || 3000);
 
 app.set('view engine', 'ejs');
@@ -19,29 +17,10 @@ app.get('/', function (req, res) {
 });
 
 io.sockets.on('connection', function(socket) {
-  socket.on('sendchat', function (data) {
-    io.sockets.emit('updatechat', socket.username, data);
-  });
-
-  socket.on('adduser', function(username) {
-    socket.username = username;
-
-    usernames[username] = username;
-
-    socket.emit('servernotification', { connected: true, to_self: true, username: username });
-
-    socket.broadcast.emit('servernotification', { connected: true, username: username });
-
-    io.sockets.emit('updateusers', usernames);
-  });
+  console.log("CONNECTED!");
 
   // when the user disconnects.. perform this
   socket.on('disconnect', function(){
-
-    delete usernames[socket.username];
-
-    io.sockets.emit('updateusers', usernames);
-
-    socket.broadcast.emit('servernotification', { username: socket.username });
+    console.log("DISCONNECTED!")
   });
 });
